df8ca44c3fb36cdc6c7c5ae3a173590e2df426dc
romulo <romuloricoy@gmail.com>
2018-08-26 16:13:37 -0300

    initial version

--- php/brl-usd.php
+++ php/brl-usd.php
@@ -4,13 +4,18 @@ require_once __DIR__ . '/vendor/autoload.php';
 use \NodePHPTest\BRLExchange;
 $app = new Silex\Application();
 
-$app->get('/', function() use ($app, $urlAPI) {
+$app->get('/', function() use ($app) {
     $price = BRLExchange::getDollarPrice();
 
-    return $app->json([
-        'brl' => 1,
-        'usd' => (float) number_format($price->value[0]->cotacaoVenda, 2, '.', '')
-    ]);
+    if(!empty($price->value)) {
+        return $app->json([
+            'brl' => 1,
+            'usd' => (float) number_format($price->value[0]->cotacaoVenda, 2, '.', '')
+        ]);
+    }
+    else {
+        return $app->json([]);
+    }
 });
 
 $app->run();
